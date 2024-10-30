7cdf458154e676808c65e793edacc69d738069d2
Crystalforest10 <130143895+Crystalforest10@users.noreply.github.com>
2024-10-30 20:00:12 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,33 +2,33 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 /*
-	* Create By Naze
-	* Follow https://github.com/nazedev
-	* Whatsapp : wa.me/6282113821188
+	* Create By Dark System
+	* Follow https://github.com/Crystalforest10
+	* Whatsapp : wa.me/6281242770220
 */
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188','6281356484612','6285773557984']
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6281242770220','6282242597213']
+global.packname = 'Change Lol'
+global.author = 'Zahran'
+global.botname = 'Dark System'
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
 global.pairing_code = true
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnailUrl: 'https://i.ibb.co.com/9y6tWCT/59ad8ea1fc5d82dd4a14d5325659e69c.jpg',
 	thumbnail: fs.readFileSync('./src/media/naze.png'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/Hx9vcBVhbc04KLVGPFtH2R',
+	yt: 'https://youtube.com/',
+	gh: 'https://github.com/',
+	gc: 'https://chat.whatsapp.com/',
 	ch: '120363250409960161@newsletter',
 }
 
